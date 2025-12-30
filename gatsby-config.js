7ef3e5aa18d2403e0220663115f804af21f7ae50
EvansOdhams <evansodhams@gmail.com>
2025-12-30 21:06:18 +0300

    chore: replace gatsby-plugin-google-analytics with gatsby-plugin-google-gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -242,9 +242,21 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: 'UA-45666519-2',
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'UA-45666519-2', // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: [],
+        },
       },
     },
   ],
