7097aff8b017474b291112f52dbc7ac4924ab33e
EvansOdhams <evansodhams@gmail.com>
2025-12-30 23:32:30 +0300

    Fix configs and resolve dependency conflicts

--- prettier.config.js
+++ prettier.config.js
@@ -1,1 +1,9 @@
-module.exports = require('@upstatement/prettier-config');
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  printWidth: 100,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+};
