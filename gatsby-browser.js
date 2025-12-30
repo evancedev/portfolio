047036a7f54e5150ea229914241663c172a4e1c3
EvansOdhams <evansodhams@gmail.com>
2025-12-30 20:13:21 +0300

    Solve Deployment errors

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,28 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
+
+import React from 'react';
+import { GlobalStyle } from './src/styles';
+import Layout from './src/components/layout';
+
+export const wrapPageElement = ({ element, props }) => (
+  <>
+    <GlobalStyle />
+    <Layout {...props}>{element}</Layout>
+  </>
+);
+
+// Prevent default scroll restoration to fix page transitions
+const scrollTo = id => {
+  const element = document.getElementById(id);
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+};
+
+export const onRouteUpdate = ({ location: { hash } }) => {
+  if (hash) {
+    window.setTimeout(() => scrollTo(hash.substr(1)), 0);
+  }
+};
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,4 +4,13 @@
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
 
-// You can delete this file if you're not using it
+import React from 'react';
+import { GlobalStyle } from './src/styles';
+import Layout from './src/components/layout';
+
+export const wrapPageElement = ({ element, props }) => (
+  <>
+    <GlobalStyle />
+    <Layout {...props}>{element}</Layout>
+  </>
+);
